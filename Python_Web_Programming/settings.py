b4668970ceebd60ddb1a96cce6592545c979e8ca
boris-minsk <boris_sigal@earthlink.net>
2021-10-12 10:08:37 +0200

    App added.

--- Python_Web_Programming/settings.py
+++ Python_Web_Programming/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'PersonalWebSite.apps.PersonalwebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
